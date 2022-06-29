9588302fe14583cac2610c93865c5e394fa49639
Squirrel9636 <108181106+Squirrel9636@users.noreply.github.com>
2022-06-29 19:55:37 +0600

    Add files via upload

---
+++ Potfolio/js/script.js
@@ -0,0 +1,28 @@
+﻿
+function Link(link) {
+    location.href = link;
+}
+function scroll(X, Y) {
+    window.scrollTo(X, Y);
+}
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+
+
+        }
+        else {
+            reveals[i].classList.remove('active');
+
+        }
+    }
+}
+reveal();
